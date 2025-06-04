0af7df37597e0c2ef2caad0e9bf33289523db133
ram-kumar-06 <ramkumar.togapurkar@version1.com>
2025-06-04 10:28:43 -0800

    first commit

---
+++ src/main/java/com/performance/kpis/projectadmin/ProjectAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.performance.kpis.projectadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAdminServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performance/kpis/projectadmin/ProjectAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performance.kpis.projectadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
